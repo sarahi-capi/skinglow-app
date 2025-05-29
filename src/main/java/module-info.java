42e845e29c8f824611f69033bd7b1d59402d6fa7
Sarahi Capi Casillas <s240349@th-ab.de>
2025-05-29 17:16:39 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.skinglow {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.skinglow to javafx.fxml;
+    exports com.example.skinglow;
+}
