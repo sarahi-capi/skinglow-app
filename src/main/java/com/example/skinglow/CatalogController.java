adc65edf6201cb7b069374abfb3fb5f536eca79e
Sarahi Capi Casillas <s240349@th-ab.de>
2025-05-29 22:36:20 +0200

    Adding filtering buttons to the Catalog Scene

--- src/main/java/com/example/skinglow/CatalogController.java
+++ src/main/java/com/example/skinglow/CatalogController.java
@@ -8,11 +8,28 @@ import javafx.scene.image.ImageView;
 
 public class CatalogController {
     @FXML private ImageView imageLogo;
+    @FXML private ImageView allProducts;
+    @FXML private ImageView brandIcon;
+    @FXML private ImageView skinConcernIcon;
+    @FXML private ImageView cleanserIcon;
+    @FXML private ImageView tonerIcon;
+    @FXML private ImageView moisturizerIcon;
+    @FXML private ImageView serumIcon;
+    @FXML private ImageView sunscreenIcon;
+
     @FXML private TextField searchField;
 
     public void initialize() {
-        if (imageLogo != null) {
+        if (allProducts != null && imageLogo != null) {
             imageLogo.setImage(new Image(getClass().getResourceAsStream("/images/SkinCareLogo.png")));
+            allProducts.setImage(new Image(getClass().getResourceAsStream("/images/AllProducts.png")));
+            brandIcon.setImage(new Image(getClass().getResourceAsStream("/images/Brand.png")));
+            skinConcernIcon.setImage(new Image(getClass().getResourceAsStream("/images/SkinConcern.png")));
+            cleanserIcon.setImage(new Image(getClass().getResourceAsStream("/images/Cleanser.png")));
+            tonerIcon.setImage(new Image(getClass().getResourceAsStream("/images/Toner.png")));
+            moisturizerIcon.setImage(new Image(getClass().getResourceAsStream("/images/Moisturizer.png")));
+            serumIcon.setImage(new Image(getClass().getResourceAsStream("/images/Serum.png")));
+            sunscreenIcon.setImage(new Image(getClass().getResourceAsStream("/images/Sunscreen.png")));
         }
     }
 
